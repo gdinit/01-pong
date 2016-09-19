049212f551ef5d1c71418ac9d6aaecb322312677
gdinit <gdinit@gmx.com>
2016-09-19 23:01:23 +0100

    add utility.cc magnitude & magnitudeSquared functions

--- source/utility.cc
+++ source/utility.cc
@@ -8,5 +8,26 @@ void centerOrigin( sf::Text &text )
 	text.setOrigin( std::floor( bounds.left + bounds.width / 2.f ), std::floor( bounds.top + bounds.height / 2.f ) );
 }
 
+// find length (or Magnitude)
+int magnitude( int x, int y )
+{
+	return( sqrt( x * x + y * y ) );
+}
+
+float magnitude( float x, float y )
+{
+	return( sqrt( x * x + y * y ) );
+}
+
+int magnitudeSquared( int x, int y )
+{
+	return( x * x + y * y );
+}
+
+float magnitudeSquared( float x, float y )
+{
+	return( x * x + y * y );
+}
+
 /* EOF */
 
